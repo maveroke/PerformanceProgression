a06b9c514ad4fa813161ef6ddf9246c209120d04
Michael <maveroke55@hotmail.com>
2012-06-11 20:56:11 +1200

    Math Program Added. Still needs Inverse, Multiply and Equation

---
+++ GraphCalculator/Attempt1MathCalculation/Attempt1MathCalculation/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Attempt1MathCalculation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            float[] iper_x = { 1,2,3,4};
+            float[] iper_y = { 1,2,3,4};
+            SecondOrderPolynomial(iper_x, iper_y);
+        }
+        /// <summary>
+        /// takes 2 float arrays of all x,y values
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public float SecondOrderPolynomial(float[] x, float[] y)
+        {
+
+            //initialise the matrix
+            Matrix3x3(x);
+
+
+            float[] MatrixB = Matrix1x3(x, y);
+
+            //returns a b c in a array
+            float iper = y.Sum();
+            return iper;
+        }
+
+        private float[,] Matrix3x3(float[] x)
+        {
+            float[,] MatrixA = new float[,]{
+                {0, 0, 0},
+	            {0, 0, 0},
+	            {0, 0, 0},
+            };
+
+
+            float temp_xx = 0;
+            float temp_xxx = 0;
+            float temp_xxxx = 0;
+
+            foreach (float value in x)
+            {
+                temp_xx += value * value;
+                temp_xxx += value * value * value;
+                temp_xxxx += value * value * value * value;
+            }
+            MatrixA[0, 0] = x.Count();
+            MatrixA[1, 0] = x.Sum();
+            MatrixA[2, 0] = temp_xx;
+            MatrixA[0, 1] = x.Sum();
+            MatrixA[1, 1] = temp_xx;
+            MatrixA[2, 1] = temp_xxx;
+            MatrixA[0, 2] = temp_xx;
+            MatrixA[1, 2] = temp_xxx;
+            MatrixA[2, 2] = temp_xxxx;
+
+            return MatrixA;
+        }
+
+        private float[] Matrix1x3(float[] x, float[] y)
+        {
+            float[] MatrixB = new float[] { 0, 0, 0 };
+            float temp = 0;
+            foreach (float x_val in x)
+            {
+                temp += x_val * x_val;
+            }
+            MatrixB[0] = y.Sum();
+            MatrixB[1] = y.Sum() * x.Sum();
+            MatrixB[2] = temp * y.Sum();
+            return MatrixB;
+        }
+    }
+}
---
+++ GraphCalculator/Attempt1MathCalculation/MathTest/Form1Test.cs
@@ -0,0 +1,127 @@
+﻿using Attempt1MathCalculation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+namespace MathTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for Form1Test and is intended
+    ///to contain all Form1Test Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class Form1Test
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for SecondOrderPolynomial
+        ///</summary>
+        [TestMethod()]
+        public void SecondOrderPolynomialTest()
+        {
+            Form1 target = new Form1(); // TODO: Initialize to an appropriate value
+            float[] iper = { 1,2,3,4};
+            float[] SumOf = {3376};
+            float test = target.SecondOrderPolynomial(iper,iper);
+
+                Assert.AreEqual(test,10);
+        }
+
+        /// <summary>
+        ///A test for Matrix1x3
+        ///</summary>
+        [TestMethod()]
+        public void Matrix1x3Test()
+        {
+            Form1_Accessor target = new Form1_Accessor(); // TODO: Initialize to an appropriate value
+            float[] x = { 1, 2, 3, 4}; // TODO: Initialize to an appropriate value
+            float[] y = { 1, 2, 3, 4}; // TODO: Initialize to an appropriate value
+            float[] temp = target.Matrix1x3(x, y);
+            float[] check = { 10,100,300};
+            int p = 0;
+            foreach (float i in temp)
+            {
+                Assert.AreEqual(i, check[p]);
+                p++;
+            }
+        }
+
+        /// <summary>
+        ///A test for Matrix3x3
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("Attempt1MathCalculation.exe")]
+        public void Matrix3x3Test()
+        {
+            Form1_Accessor target = new Form1_Accessor(); // TODO: Initialize to an appropriate value
+            float[] x = { 1, 2, 3, 4 }; // TODO: Initialize to an appropriate value
+
+            float[,] expected = new float[,]{
+                {4,  10, 30},
+	            {10, 30, 100},
+	            {30, 100, 354},
+            };
+
+            float[,] actual = target.Matrix3x3(x);
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Assert.AreEqual(expected[i, j], actual[i, j]);
+                }
+            }
+        }
+    }
+}
