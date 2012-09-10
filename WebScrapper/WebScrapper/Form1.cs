68952cd0b59b2ae33df303cad152e37b88c05268
Michael <maveroke55@hotmail.com>
2012-09-10 17:06:23 +1200

    f

--- WebScrapper/WebScrapper/Form1.cs
+++ WebScrapper/WebScrapper/Form1.cs
@@ -18,13 +18,13 @@ namespace WebScrapper
         //protected Uri ur = new Uri("http://www.ThisIsWhyImBroke.com/");
         string athleteBirthDate = "";
         int ageOfCollection;
-        int nameChange;
+        bool treset = false;
         string nameOfAthlete;
 
         public Form1()
         {
             InitializeComponent();
-            
+
             //WebBrowser browser = new WebBrowser();
             //browser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(browser_DocumentCompleted);
             webBrowser1.ScriptErrorsSuppressed = true;
@@ -40,23 +40,14 @@ namespace WebScrapper
 
         private void button5_Click(object sender, EventArgs e)
         {
-            int position = AgeStartingPoint();
+
             foreach (HtmlElement asd in webBrowser1.Document.All.GetElementsByName("Name"))
             {
                 asd.InnerText = textBox1.Text;
             }
+
             accept();
-            Uri theAthletePage = webBrowser1.Url;
-
-            for (; position > 0; position--)
-            {
-                YearCollection yc = new YearCollection();
-                yc.athletePage = theAthletePage;
-                yc.position = position;
-                Thread thrd = new Thread(new ThreadStart(yc.begin));
-                thrd.Name = nameOfAthlete +" "+ position;
-
-            }
+            treset = true;
         }
         /// <summary>
         /// Collects the age at the top of the page
@@ -77,7 +68,7 @@ namespace WebScrapper
             int startAge = Convert.ToInt32(result);
             ageOfCollection = startAge + 1910;
             return DateTime.Today.Year - ageOfCollection + 1;
-        }        
+        }
 
         private void accept()
         {
@@ -89,6 +80,87 @@ namespace WebScrapper
                     break;
                 }
             }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (treset)
+            {
+                MessageBox.Show("");
+                Uri theAthletePage = webBrowser1.Url;
+                int position = AgeStartingPoint();
+                for (; position > 0; position--)
+                {
+                    YearCollection yc = new YearCollection();
+                    yc.athletePage = theAthletePage;
+                    yc.position = position;
+                    Thread thrd = new Thread(new ThreadStart(yc.begin));
+                    thrd.Name = nameOfAthlete + " " + position;
+                    thrd.Start();
+                }
+            }
+        }
+    }
+    class YearCollection
+    {
+        int nameChange;
+        WebBrowser webBrowser = new WebBrowser();
+        public Uri athletePage;
+        public int position;
+        private bool treset = false;
+        public void begin()
+        {
+            Thread thr = Thread.CurrentThread;
+            webBrowser.Navigate(athletePage);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////WebClient
+            //google C# using threads how do i open multiple webbrowsers
+            webBrowser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser_DocumentCompleted);
+            System.Diagnostics.Debug.WriteLine(thr.Name + " has begun threading");
+
+            foreach (HtmlElement asd in webBrowser.Document.All)
+            {
+                if (asd.GetAttribute("name").Equals("menupi9"))
+                {
+                    NavigateToAgePage(position, asd);
+                    break;
+                }
+            }
+            System.Diagnostics.Debug.WriteLine(thr.Name + " is navigating to its page");
+            treset = true;
+        }
+        void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            Thread thr = Thread.CurrentThread;
+            if (treset)
+            {
+                System.Diagnostics.Debug.WriteLine(thr.Name + " has navigated");
+                CollectData();
+            }
+        }
+        private void NavigateToAgePage(int position, HtmlElement asd)
+        {
+            nameChange = position;
+            asd.Children[position].SetAttribute("selected", "x");
+            asd.RaiseEvent("onchange");
+        }
+
+
+        public void CollectData()
+        {
+            Thread thr = Thread.CurrentThread;
+            System.Diagnostics.Debug.WriteLine(thr.Name + " is working!");
+
+            string result = webBrowser.DocumentText;
+            int startIndex = result.IndexOf("help: click on placing to see event results in the competition.") + 64;
+            int endIndex = result.IndexOf("</td></table></td></tr>", startIndex);
+
+            string end = result.Substring(startIndex, endIndex - startIndex);
+
+            StreamWriter sw = new StreamWriter(thr.Name + ".txt");
+            sw.Write(end);
+            sw.Close();
+
+            System.Diagnostics.Debug.WriteLine(thr.Name + " FINISHED!");
         }
     }
 }
